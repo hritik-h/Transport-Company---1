4bb2221a5aa725e5a49c5db2e8676a6971b0a3a8
hritik-h <hritik.bnp@gmail.com>
2022-08-09 23:00:47 +0530

    filter-1

---
+++ Trasport Company - 1/src/main/java/filter/ValidationFilter.java
@@ -0,0 +1,62 @@
+package filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class ValidationFilter
+ */
+@WebFilter("/login")
+public class ValidationFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public ValidationFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		PrintWriter out = response.getWriter();
+		// TODO Auto-generated method stub
+		// place your code here
+			
+		// pass the request along the filter chain
+		HttpServletRequest req = (HttpServletRequest) request;
+		String uname = request.getParameter("username");
+		String upassword = request.getParameter("password");
+		
+		if (uname==null || uname.equals("")||upassword==null || upassword.equals(""))
+			out.println("Bad Credentials");
+		else chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
